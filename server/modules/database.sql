fb20a9f8fdde9ef5f4a35d6ed2f41ef107dc98e6
emsweetland <emmaksweetland@gmail.com>
2022-08-06 13:33:00 -0500

    sql notes

--- server/modules/database.sql
+++ server/modules/database.sql
@@ -1,8 +1,18 @@
 CREATE TABLE "tasks" (
 	"id" SERIAL PRIMARY KEY,
-	"task" VARCHAR(100) NOT NULL,
+	"task" VARCHAR(100) NOT NULL, 
 	"status" BOOLEAN DEFAULT false
 	);
 	
 INSERT INTO tasks (task)
 VALUES ('task1'), ('task2'), ('task3');
+
+id automatically sets with each task
+task must be shorter than 100 characters, cannot be left blank
+status boolean to toggle complete/incomplete,
+     default false = incomplete
+
+POST will only reqiure the task,
+GET will return id, task, and status
+
+
